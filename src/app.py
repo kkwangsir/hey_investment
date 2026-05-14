615e8fdf48229c1773998d837caa43e4eee15269
kkwangsir <kkwangsir@gmail.com>
2026-05-14 00:41:41 +0000

    change port 8080→8000

--- src/app.py
+++ src/app.py
@@ -62,4 +62,4 @@ async def index(request: Request):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=False)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=False)
